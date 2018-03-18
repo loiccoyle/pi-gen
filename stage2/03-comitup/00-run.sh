e8669ed194f19eabc2d117426b97ffef9a1d5047
David Steele <dsteele@gmail.com>
2018-03-17 21:16:52 -0400

    Add comitup processing to stage 2

---
+++ stage2/03-comitup/00-run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -e
+
+touch ${ROOTFS_DIR}/boot/ssh
+
+rm -f ${ROOTFS_DIR}/etc/network/interfaces
+install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/
+
+install -m 644 files/comitup.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
+on_chroot apt-key add - < files/key-366150CE.pub.txt
+on_chroot << EOF
+apt-get update
+EOF
+
