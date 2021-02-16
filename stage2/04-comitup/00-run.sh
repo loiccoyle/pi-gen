aae17290983e20bda1477540731dbba17affc544
David Steele <steele@debian.org>
2021-02-16 16:03:54 -0500

    Use the comitup repo key deb file

--- stage2/04-comitup/00-run.sh
+++ stage2/04-comitup/00-run.sh
@@ -7,9 +7,10 @@ mv ${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant.conf ${ROOTFS_DIR}/etc/wpa_su
 rm -f ${ROOTFS_DIR}/etc/network/interfaces
 install -m 644 files/interfaces ${ROOTFS_DIR}/etc/network/
 
-install -m 644 files/comitup.list ${ROOTFS_DIR}/etc/apt/sources.list.d/
-on_chroot apt-key add - < files/key-366150CE.pub.txt
+cp files/davesteele-comitup-apt-source*deb ${ROOTFS_DIR}/tmp/
 on_chroot << EOF
+dpkg -i /tmp/davesteele-comitup-apt-source*deb
+rm -f /tmp/davesteele-comitup-apt-source*deb
 apt-get update
 systemctl mask dnsmasq.service
 systemctl mask systemd-resolved.service
