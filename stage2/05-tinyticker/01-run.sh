bc3ea93d3f12da7233826e6dec99e4f94705c113
Loic Coyle <loic.coyle@hotmail.fr>
2021-11-24 00:04:35 +0100

    set CFLAGS to fix rpi.gpio compilation error

--- stage2/05-tinyticker/01-run.sh
+++ stage2/05-tinyticker/01-run.sh
@@ -6,6 +6,6 @@ on_chroot <<EOF
 sed -i "s/# ap_name: comitup-<nnn>/ap_name: ${TARGET_HOSTNAME}/" /etc/comitup.conf
 sudo -u ${FIRST_USER_NAME} python3 -m pip install --user pipx
 sudo -u ${FIRST_USER_NAME} python3 -m pipx ensurepath
-sudo -u ${FIRST_USER_NAME} python3 -m pipx install --system-site-packages tinyticker
+sudo -u ${FIRST_USER_NAME} CLFAGS=-fcommon python3 -m pipx install tinyticker
 sudo -u ${FIRST_USER_NAME} /home/${FIRST_USER_NAME}/.local/bin/tinyticker --start-on-boot
 EOF
