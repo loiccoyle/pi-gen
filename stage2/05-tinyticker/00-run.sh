4d107e35bab2cb35f21970e76776e1bc532191ec
Loic Coyle <loic.coyle@hotmail.fr>
2022-07-10 18:04:11 +0100

    overhaul tinyticker setup

--- stage2/05-tinyticker/00-run.sh
+++ stage2/05-tinyticker/00-run.sh
@@ -2,7 +2,13 @@
 
 set -e
 
-useradd -m tinyticker -G users,sudo,gpio,spi,i2c
+on_chroot <<EOF
+id -u tinyticker || useradd -m tinyticker -G users,sudo,gpio,spi,i2c -p OlWmFBPK9SgIg
+echo "tinyticker:tinyticker" | chpasswd
+mkdir -p /var/lib/systemd/linger/
+touch /var/lib/systemd/linger/tinyticker
+echo "tinyticker ALL=(ALL) NOPASSWD:ALL" >> /etc/sudoers
+EOF
 
 wget -P "${ROOTFS_DIR}/tmp" 'http://www.airspayce.com/mikem/bcm2835/bcm2835-1.60.tar.gz'
 
--- stage2/05-tinyticker/01-run.sh
+++ stage2/05-tinyticker/01-run.sh
@@ -4,8 +4,22 @@ set -e
 
 on_chroot <<EOF
 sed -i "s/# ap_name: comitup-<nnn>/ap_name: ${TARGET_HOSTNAME}/" /etc/comitup.conf
-sudo -u tinyticker python3 -m pip install --user pipx
-sudo -u tinyticker python3 -m pipx ensurepath
-sudo -u tinyticker python3 -m pipx install tinyticker
-sudo -u tinyticker /home/tinyticker/.local/bin/tinyticker --start-on-boot
+sudo -u tinyticker python3 -m pip install --user tinyticker
+EOF
+
+
+on_chroot <<EOF
+sudo -u tinyticker mkdir -p /home/tinyticker/.config/systemd/user/
+EOF
+install -m 644 files/*.service "${ROOTFS_DIR}/home/tinyticker/.config/systemd/user/"
+on_chroot <<EOF
+chown tinyticker:tinyticker /home/tinyticker/.config/systemd/user/*
+EOF
+
+install -m 644 files/90-unpriviledge-port.conf "${ROOTFS_DIR}/etc/sysctl.d/"
+
+on_chroot <<EOF
+sudo -u tinyticker systemctl enable --user tinyticker.service
+sudo -u tinyticker systemctl enable --user tinyticker-qrcode.service
+sudo -u tinyticker systemctl enable --user tinyticker-web.service
 EOF
