495a65cdca80b8db39e27f1d121cef9fe0066db7
Loic Coyle <loic.coyle@hotmail.fr>
2024-01-10 21:57:24 +0100

    fix(tinyticker): update build pkgs & force pip to install with `--break-system-packages`

--- stage2/05-tinyticker/01-run.sh
+++ stage2/05-tinyticker/01-run.sh
@@ -4,9 +4,8 @@ set -e
 
 on_chroot <<EOF
 sed -i "s/# ap_name: comitup-<nnn>/ap_name: ${TARGET_HOSTNAME}/" /etc/comitup.conf
-sudo -u tinyticker python3 -m pip install --user tinyticker
+sudo -u tinyticker python3 -m pip install --user --break-system-packages tinyticker
 EOF
-
 
 on_chroot <<EOF
 sudo -u tinyticker mkdir -p /home/tinyticker/.config/systemd/user/
