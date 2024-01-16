c11bb699e387850308f1f067cbbf46485a4a588d
Loic Coyle <loic.coyle@hotmail.fr>
2024-01-16 15:25:23 +0100

    fix: create log dir when seting up the img not when starting services

--- stage2/05-tinyticker/01-run.sh
+++ stage2/05-tinyticker/01-run.sh
@@ -2,11 +2,13 @@
 
 set -e
 
+# install tinyticker
 on_chroot <<EOF
 sed -i "s/# ap_name: comitup-<nnn>/ap_name: ${TARGET_HOSTNAME}/" /etc/comitup.conf
 sudo -u tinyticker python3 -m pip install --user --break-system-packages tinyticker
 EOF
 
+# copy over service files
 on_chroot <<EOF
 sudo -u tinyticker mkdir -p /home/tinyticker/.config/systemd/user/
 EOF
@@ -15,10 +17,13 @@ on_chroot <<EOF
 chown tinyticker:tinyticker /home/tinyticker/.config/systemd/user/*
 EOF
 
+# open up port 80 for tinyticker
 install -m 644 files/90-unpriviledge-port.conf "${ROOTFS_DIR}/etc/sysctl.d/"
 
+# enable service files and create log dir
 on_chroot <<EOF
 sudo -u tinyticker systemctl enable --user tinyticker.service
 sudo -u tinyticker systemctl enable --user tinyticker-qrcode.service
 sudo -u tinyticker systemctl enable --user tinyticker-web.service
+sudo -u tinyticker mkdir -p /home/tinyticker/.local/share/tinyticker/
 EOF
