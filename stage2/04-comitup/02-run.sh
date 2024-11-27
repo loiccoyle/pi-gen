01eca1d3eebfcd496b4db7f108aca70c5e41f556
David Steele <steele@debian.org>
2024-11-27 14:32:36 -0500

    Add a service to enable wifi on startup
    Recent images disable wifi if the regulatory domain is not set. Add
    a systemd service that calls nmcli to turn on the devices.

--- stage2/04-comitup/02-run.sh
+++ stage2/04-comitup/02-run.sh
@@ -8,7 +8,10 @@ done
 
 install -m 755 files/set_comitup_hostname_once "${ROOTFS_DIR}/etc/init.d/"
 
+install -m 644 files/wifi-on.service "${ROOTFS_DIR}/etc/systemd/system/"
+
 on_chroot <<EOF
 systemctl enable set_comitup_hostname_once
+systemctl enable wifi-on.service
 systemctl enable NetworkManager.service
 EOF
